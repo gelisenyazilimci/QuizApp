4787779d290c053a27ddbddbe50034e62a144376
gelisenyazilimci <burbytes@gmail.com>
2024-12-08 23:26:38 +0300

    Final

--- JS/app.js
+++ JS/app.js
@@ -8,19 +8,100 @@ const soruListesi = [
 const quiz = new Quiz(soruListesi);
 const ui = new UI();
 
-document.getElementById("btnSoruGetir").addEventListener("click", function() {
-    if(quiz.sorular.length != quiz.soruIndex) {
-        ui.soruGoster(quiz.soruGetir());
-        quiz.soruIndex += 1;
-
-        console.log(quiz);
-
-    } else {
-        console.log("quiz bitti");
-    }
-
+ui.btnStart.addEventListener("click", function() {
+    startTimer(10);
+    startTimerLine();
+    ui.quizBox.classList.add("active");
+    ui.buttonBox.classList.remove("active");
+    ui.soruGoster(quiz.soruGetir());
+    ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+    ui.btnNext.classList.remove("show");
 });
 
+ui.btnNext.addEventListener("click", function() {
+    if(quiz.sorular.length != quiz.soruIndex) {
+        startTimer(10);
+        startTimerLine();
+        ui.soruGoster(quiz.soruGetir());
+        ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+        ui.btnNext.classList.remove("show");
+    } else {
+        ui.scoreBox.classList.add("active");
+        ui.quizBox.classList.remove("active");
+        ui.skoruGoster(quiz.dogruCevapSayisi, quiz.sorular.length);
+    }
+});
 
+function optionSelected(e) {
+    clearInterval(counter);
+    clearInterval(counterLine);
+    let selectedElement = e.target;
 
+    if(selectedElement.nodeName == "SPAN") {
+        selectedElement = selectedElement.parentElement;
+    }
+    const cevap = e.target.textContent[0];
+    const soru = quiz.soruGetir();
 
+    if(soru.cevabiKontrolEt(cevap)) {
+        quiz.dogruCevapSayisi += 1;
+        selectedElement.classList.add("correct");
+        selectedElement.insertAdjacentHTML("beforeend", ui.correctIcon);
+    } else {
+        selectedElement.classList.add("incorrect");
+        selectedElement.insertAdjacentHTML("beforeend", ui.inCorrectIcon);
+    }
+
+    quiz.soruIndex += 1;
+    ui.disableAllOption();
+    ui.btnNext.classList.add("show");
+}
+
+ui.btnQuit.addEventListener("click", function() {
+    window.location.reload();
+});
+
+ui.btnReplay.addEventListener("click", function() {
+    quiz.soruIndex = 0;
+    quiz.dogruCevapSayisi = 0;
+    // start button
+    ui.btnStart.click();
+    ui.scoreBox.classList.remove("active");
+});
+
+let counter;
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+
+    function timer() {
+        ui.timeSecond.textContent = time;
+        time--;
+
+        if(time < 0) {
+            clearInterval(counter);
+            ui.timeText.textContent = "Süre Bitti";
+
+            ui.disableAllOption();
+            quiz.soruIndex += 1;
+
+            ui.btnNext.classList.add("show");
+        }
+    }
+}
+
+let counterLine;
+function startTimerLine() {
+    let line_width = 0;
+
+    counterLine = setInterval(timer, 20);
+
+    function timer() {
+        line_width += 1;
+
+        ui.timeLine.style.width = line_width + "px";
+
+        if(line_width > 549) {
+            clearInterval(counterLine);
+        }
+    }
+}
--- JS/ui.js
+++ JS/ui.js
@@ -1,6 +1,17 @@
 function UI() {
-    this.quiz_box = document.querySelector("#quiz-box");
+    this.quizBox = document.querySelector("#quiz-box");
+    this.buttonBox = document.querySelector("#button-box");
+    this.scoreBox = document.querySelector("#score-box");
     this.body = document.querySelector("#quiz-box #body");
+    this.correctIcon = '<i class="bi bi-check-circle"></i>';
+    this.inCorrectIcon = '<i class="bi bi-x-circle"></i>';
+    this.btnStart = document.querySelector(".btn-start");
+    this.btnNext = document.querySelector(".btn-next");
+    this.btnReplay = document.querySelector(".btn-replay");
+    this.btnQuit = document.querySelector(".btn-quit");
+    this.timeText = document.querySelector(".time-text");
+    this.timeSecond = document.querySelector(".time-second");
+    this.timeLine = document.querySelector(".time-line");
 }
 
 UI.prototype.soruGoster = function(soru) {
@@ -19,6 +30,7 @@ UI.prototype.soruGoster = function(soru) {
     for(let [key,value] of Object.entries(soru.cevapSecenekleri)) {
         const option = document.createElement("div");
         option.classList.add("option");
+        option.addEventListener("click", optionSelected);
 
         const span = document.createElement("span");
         span.textContent = key + ") " + value;
@@ -31,4 +43,21 @@ UI.prototype.soruGoster = function(soru) {
     cardBody.appendChild(optionList);
 
     this.body.appendChild(cardBody);
+}
+
+UI.prototype.disableAllOption = function() {
+    const options = document.querySelectorAll(".option");
+    for(let option of options) {
+        option.classList.add("disabled");
+    }
+}
+
+UI.prototype.soruSayisiniGoster = function(soruSirasi, toplamSoru) {
+    const etiket = `<span class="badge text-bg-danger">${soruSirasi} / ${toplamSoru}</span>`;
+    document.querySelector(".question-index").innerHTML = etiket;
+}
+
+UI.prototype.skoruGoster = function(dogruCevap, toplamSoru) {
+    const etiket = `Toplam ${toplamSoru} soruda ${dogruCevap} doğru cevap verdiniz.`;
+    document.querySelector(".score-text").innerHTML = etiket;
 }
